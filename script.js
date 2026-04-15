5e074576c90699d8a7702b4ff2c330ffd81e3af7
Ayush Sahu <115640976+ayushS22@users.noreply.github.com>
2026-04-15 22:59:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+// Get button and select elements
+const select = document.getElementById("colorSelect");
+const button = document.querySelector('input[type="button"]');
+
+// Add click event
+button.addEventListener("click", function () {
+  // Get selected index
+  const selectedIndex = select.selectedIndex;
+
+  // Remove selected option
+  if (selectedIndex !== -1) {
+    select.remove(selectedIndex);
+  }
+});
